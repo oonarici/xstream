e4b3ae91bcda7169bbb33a356c45fb28d85e9fd3
Onur Onarıcı <onuronarici@gmail.com>
2019-08-30 23:45:29 +0300

    test code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 
+#include "filereader.h"
+#include "filewriter.h"
+#include "abstractreader.h"
+
 using namespace std;
 
 int main()
 {
+    //    ifstream file;
+    //    ifstream *pFile = &file;
+
+    //    file.open("/home/oonarici/Desktop/background-data.txt", std::ios::out | std::ios::in | std::ios::app);
+    //    FileReader someReader(pFile);
+
+    //    long length = someReader.length();
+    //    std::cout << "length: " << length << "\n"
+    //              << "Cursor Position: " << someReader.pos() << "\n";
+    //    someReader.seek(100);
+    //    std::cout << "Cursor Position: " << someReader.pos() << "\n";
+
+    ofstream wfile;
+    ofstream *pwFile = &wfile;
+
+    wfile.open("/home/oonarici/Desktop/background-data.txt", std::ios::out | std::ios::in | std::ios::app);
+
+    FileWriter someWriter(pwFile);
+    std::cout << someWriter.length() << "\n"
+              << someWriter.pos() << "\n";
+    someWriter.seek(3);
+
+    std::cout << someWriter.pos() << "\n";
+
 
 }
