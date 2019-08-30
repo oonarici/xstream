d3c637064236e7d8ba40baf453f22b1c401294b7
Onur Onarıcı <onuronarici@gmail.com>
2019-08-30 17:50:07 +0300

    fix some bugs

--- abstractwriter.cpp
+++ abstractwriter.cpp
@@ -1,5 +1,11 @@
 #include "abstractwriter.h"
 
+void AbstractWriter::write(const bool &v)
+{
+    const void *ptr = &v;
+    this->write(ptr, sizeof(bool));
+}
+
 void AbstractWriter::write(const unsigned int &v)
 {
     const void *ptr = &v;
